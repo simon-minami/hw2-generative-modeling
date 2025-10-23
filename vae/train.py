8d63937484f51fcc048a68868c332d2d14c61929
simon minami <simon.sho.minami@gmail.com>
2025-10-22 20:40:11 -0400

    replace sig with tanh

--- vae/train.py
+++ vae/train.py
@@ -18,14 +18,14 @@ def ae_loss(model, x):
     ##################################################################
     # TODO 2.2: Fill in MSE loss between x and its reconstruction.
     ##################################################################
-    print(f'debug: recon target min max: {torch.min(x).item(), torch.max(x).item()}')
+    # print(f'debug: recon target min max: {torch.min(x).item(), torch.max(x).item()}')
     loss = None
-    # i think we need to sigmoid to get [0,1] pixel values
+    # i think we need to tanh to get [-1,1] pixel values
     z = model.encoder(x)
     x_pred = model.decoder(z)
-    print(f'debug: decoder output before sig min max: {torch.min(x_pred).item(), torch.max(x_pred).item()}')
+    # print(f'debug: decoder output before sig min max: {torch.min(x_pred).item(), torch.max(x_pred).item()}')
 
-    x_pred = torch.sigmoid(x_pred)
+    x_pred = torch.tanh(x_pred)
 
     # now we can calc MSE
     loss = F.mse_loss(x_pred, x)
